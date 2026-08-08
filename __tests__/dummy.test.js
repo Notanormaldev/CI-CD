f446985343a9e62ff2047469c0e279a5aadf5b64
Notanormaldev <harshpatel20050@gmail.com>
2026-08-08 11:50:45 +0530

    test: add unit tests for sum function in dummy.test.js

--- __tests__/dummy.test.js
+++ __tests__/dummy.test.js
@@ -2,7 +2,7 @@ import { sum } from "../utils";
 
 describe('sum function',()=>{
     test('add 2 + 2',()=>{
-        expect(sum(2,2)).toBe(4);
+        expect(sum(2,2)).toBe(5);
     })
     test('add -1 + -1',()=>{
         expect(sum(-1,-1)).toBe(-2);
